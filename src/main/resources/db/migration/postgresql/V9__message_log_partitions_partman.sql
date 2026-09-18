e3c408d7a0e6d0c49345da4d7c95d35e0463bf86
gdinizds <guilherme_diniz2020@live.com>
2026-09-17 21:20:24 -0300

    fix: removendo pg_cron da migration, schedule via infra

--- src/main/resources/db/migration/postgresql/V9__message_log_partitions_partman.sql
+++ src/main/resources/db/migration/postgresql/V9__message_log_partitions_partman.sql
@@ -2,11 +2,11 @@ CREATE SCHEMA IF NOT EXISTS partman;
 CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
 
 SELECT partman.create_parent(
-    p_parent_table => 'gateway.message_log',
-    p_control      => 'recorded_at',
-    p_interval     => '1 day',
-    p_premake      => 3
-);
+               p_parent_table => 'gateway.message_log',
+               p_control      => 'recorded_at',
+               p_interval     => '1 day',
+               p_premake      => 3
+       );
 
 UPDATE partman.part_config
 SET retention                = '8 days',
@@ -14,11 +14,3 @@ SET retention                = '8 days',
     retention_keep_index     = FALSE,
     infinite_time_partitions = TRUE
 WHERE parent_table = 'gateway.message_log';
-
-CREATE EXTENSION IF NOT EXISTS pg_cron;
-
-SELECT cron.schedule(
-    'partman-maintenance',
-    '0 * * * *',
-    $$SELECT partman.run_maintenance(p_analyze := FALSE)$$
-);
